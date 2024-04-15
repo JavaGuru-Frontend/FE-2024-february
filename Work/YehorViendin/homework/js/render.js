916f161ca2add0f43f782272be22951c44842ff7
Satemmka <satem13@gmail.com>
2024-04-15 20:24:55 +0200

    homework 8 , added js

---
+++ Work/YehorViendin/homework/js/render.js
@@ -0,0 +1,7 @@
+document.getElementById('projects').innerHTML +=`<div class="projects__img">
+            <img src="img/imagesssss.png" alt="">
+        </div>
+        <div class="project__text">
+            <div class="title11">Calculator</div>
+            <p>Test1.</p>
+        </div>`
